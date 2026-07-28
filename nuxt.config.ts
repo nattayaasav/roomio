d121408229ef7e506e57f2e95a3bd0826a53cbf9
nattaya <nattaya190947@gmail.com>
2026-07-29 06:00:14 +0700

    feat: setup runtimeConfig to connect render backend

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
   // เพิ่มส่วนนี้เพื่อจัดการ API URL
   runtimeConfig: {
     public: {
-      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:5000'
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'https://roomio-backend.onrender.com'
     }
   }
 })
